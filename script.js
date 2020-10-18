4555f2a68ccb50b235a68ac29b34789289c8174a
Brian Smak <brian.j.smak@gmail.com>
2020-10-18 12:27:23 -0400

    added 5 day forecast, added script

---
+++ script.js
@@ -0,0 +1,10 @@
+// Declare variables for buttons and inputting forecast information
+var searchCity = $('#search-city');
+var searchBtn = $('#search');
+var clrBtn = $('#clear');
+var currentCity = $('#current-city');
+var currentTemp = $('#temp');
+var currentHumidity = $('#humidity');
+var currentWindSpeed = $('#windSpeed');
+var currentUV = $('#uv-index');
+
